e302249f27eee9fec6510d40132ce3454687d4b7
mandresuri <mandresuri@gmail.com>
2022-09-13 10:15:20 -0500

    implementacion del CRUD completo

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -1,6 +1,13 @@
 const DB = require("./db.json");
 const { saveToDatabase } = require("./utils");
 
+const getOneWorkout = (workoutId) => {
+    const workout = DB.workouts.find((workout) => workout.id === workoutId);
+    if (!workout) {
+        return;
+    }
+    return workout;
+}
 
 const getAllWorkouts = () => {
     return DB.workouts;
@@ -17,7 +24,38 @@ const createNewWorkout = (newWorkout) => {
     return newWorkout;
 };
 
+const updateOneWorkout = (workoutId, changes) => {
+    const indexForUpdate = DB.workouts.findIndex(
+        (workout) => workout.id === workoutId
+    );
+    if (indexForUpdate === -1) {
+        return;
+    }
+    const updatedWorkout = {
+        ...DB.workouts[indexForUpdate],
+        ...changes,
+        updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    };
+    DB.workouts[indexForUpdate] = updatedWorkout;
+    saveToDatabase(DB);
+    return updatedWorkout;
+}
+
+const deleteWorkout = (workoutId) => {
+    const indexForDeletion = DB.workouts.findIndex(
+        (workout) => workout.id === workoutId
+    );
+    if (indexForDeletion === -1) {
+        return;
+    }
+    DB.workouts.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
+}
+
 module.exports = {
     getAllWorkouts,
     createNewWorkout,
+    getOneWorkout,
+    updateOneWorkout,
+    deleteWorkout
 };
